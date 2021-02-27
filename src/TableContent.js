1100efeeda4bbcaf00174f5b16e9478602decd1f
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-27 12:59:13 +0200

    added row sort and pages

--- src/TableContent.js
+++ src/TableContent.js
@@ -12,15 +12,84 @@ const rowInformation = [
     { "name": "Bobby John", age: 40 }
 ]
 
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1
+    }
+    return 0
+}
+
+function getComperator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy)
+}
+
+const sortedRowInformation = (rowArray, comparetor) => {
+    const stabilizedRowArray = rowArray.map((el, index) => [el, index])
+    stabilizedRowArray.sort((a, b) => {
+        const order = comparetor(a[0], b[0])
+        if (order !== 0) return order
+        return a[1] - b[1]
+    })
+    return stabilizedRowArray.map((el) => el[0])
+}
+
 export default function TableContent() {
+    const [orderDirection, setOrderDirection] = React.useState('asc')
+    const [valueToOrderBy, setValueToOrderBy] = React.useState('name')
+    const [page, setPage] = React.useState(0)
+    const [rowsPerPage, setRowsPerPage] = React.useState(1)
+
+    const handleRequestSort = (event, property) => {
+        const isAscending = (valueToOrderBy === property && orderDirection === 'asc')
+        setValueToOrderBy(property)
+        setOrderDirection(isAscending ? 'desc' : 'asc')
+    }
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage)
+    }
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value), 10)
+        setPage(0)
+    }
 
     return (
         <>
             <TableContainer>
                 <Table>
-                    <TableHeader />
+                    <TableHeader
+                        valueToOrderBy={valueToOrderBy}
+                        orderDirection={orderDirection}
+                        handleRequestSort={handleRequestSort}
+                    />
+                    {
+                        sortedRowInformation(rowInformation, getComperator(orderDirection, valueToOrderBy)).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((person, index) => (
+                            <TableRow key={index}>
+                                <TableCell>
+                                    {person.name}
+                                </TableCell>
+                                <TableCell>
+                                    {person.age}
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
                 </Table>
             </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[1, 2]}
+                component='div'
+                count={rowInformation.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
         </>
     )
 }
--- src/TableHeader.js
+++ src/TableHeader.js
@@ -5,23 +5,30 @@ import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 
 export default function TableHeader(props) {
+
+    const { valueToOrderBy, orderDirection, handleRequestSort } = props;
+
+    const createSortHandler = (property) => (event) => {
+        handleRequestSort(event, property)
+    }
+
     return (
         <TableHead>
             <TableRow>
                 <TableCell keys='name'>
                     <TableSortLabel
-                        active={"name" === "name"}
-                        direction='asc'
-                    // onClick={createSortHandler("name")}
+                        active={valueToOrderBy === "name"}
+                        direction={valueToOrderBy === 'name' ? orderDirection : 'asc'}
+                        onClick={createSortHandler("name")}
                     >
                         Name
                     </TableSortLabel>
                 </TableCell>
                 <TableCell keys='age'>
                     <TableSortLabel
-                        active={"age" === "age"}
-                        direction='desc'
-                    // onClick={createSortHandler("age")}
+                        active={valueToOrderBy === "age"}
+                        direction={valueToOrderBy === 'age' ? orderDirection : 'asc'}
+                        onClick={createSortHandler("age")}
                     >
                         Age
                     </TableSortLabel>
